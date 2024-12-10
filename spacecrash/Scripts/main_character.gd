4b2d89d58a60b5d0ddc7b2854f2be1bddb17248d
LukaszWilk1 <lukasz.wilk231@gmail.com>
2024-12-10 23:11:07 +0100

    jitter fixed

--- spacecrash/Scripts/main_character.gd
+++ spacecrash/Scripts/main_character.gd
@@ -6,7 +6,7 @@ const JUMP_VELOCITY = -400.0
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 
-func _physics_process(delta: float) -> void:
+func _process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
